7e1b56a140afe665cd5b1cb6dbdf988f22ad3381
danny-moloney <36445338+danny-moloney@users.noreply.github.com>
2020-10-20 22:11:10 +0100

    Add files via upload

---
+++ src/components/create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// This class stores the create component. 
+export class Create extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>This is my create component</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/movies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {MovieItem} from './movieItem';
+
+// This class stores the movies component. 
+export class Movies extends React.Component{
+
+    render(){
+        return this.props.movies.map((movie)=>{
+            return <MovieItem movie={movie}></MovieItem>
+
+        })
+    }
+}
+{/*The movieItem tag is used to imbed the movie informstion into the movie page on the app */}
